be7c3ec64def6c6c60f611cc62b642dc69a4ed35
jason65 <jsj080131@163.com>
2016-10-10 09:20:00 +0800

    sass version for bus stop

---
+++ index.js
@@ -0,0 +1,25 @@
+        function front576() {
+    		$('#front576').toggleClass('bus-front');
+    		$('#front576').toggleClass('bus-front-click');
+    		$('#back576').toggleClass('bus-back');
+    		$('#back576').toggleClass('bus-back-click');
+    	}
+    	function back576() {
+    		$('#back576').toggleClass('bus-back-click');
+    		$('#back576').toggleClass('bus-back');
+    		$('#front576').toggleClass('bus-front-click');
+    		$('#front576').toggleClass('bus-front');
+    	}
+
+        function front526() {
+            $('#front526').toggleClass('bus-front');
+            $('#front526').toggleClass('bus-front-click');
+            $('#back526').toggleClass('bus-back');
+            $('#back526').toggleClass('bus-back-click');
+        }
+        function back526() {
+            $('#back526').toggleClass('bus-back-click');
+            $('#back526').toggleClass('bus-back');
+            $('#front526').toggleClass('bus-front-click');
+            $('#front526').toggleClass('bus-front');
+        }
